9051f62271b3ba277309fe21d2d3fc66f3b1edb4
барахтин Сергей <smoke13tub@gmail.com>
2025-09-03 20:36:51 +0800

    добавленны коментарии к функциям

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from src import masks
+from src import widget
 
 input_numbers_card = input("Номер карты: ")
 input_account_number = input("Номер счета: ")
 
 print(f"Маска номера карты: {masks.get_mask_card_number(input_numbers_card)}")
 print(f"Маска номера счета: {masks.get_mask_account(input_account_number)}")
+
+print("Введите номер счета или карты")
+input_numbers_account = input("В формате <имя счета или карты> <номер карты или счета>: ")
+print(widget.mask_account(input_numbers_account))
+
+print("Введите дату")
+format_date = input("В формате <2025-01-01T00:00:00.000000>: ")
+print(widget.get_date(format_date))
--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
-def mask_account_card(number: str) -> str:
+def mask_account(number: str) -> str:
+    '''Функия накладывает маску на номер карты или счета в зависимости от длины номера'''
     separate_number = number.split()
     if len(separate_number[-1]) == 16:
         number_card_mask = f"{separate_number[-1][:6]}{"*" * 6}{separate_number[-1][-4:]}"
@@ -9,6 +10,7 @@ def mask_account_card(number: str) -> str:
 
 
 def get_date(date: str) -> str: #"2024-03-11T02:26:18.671407"
+    '''Функция переводит дату из одного формата в другой'''
     separate_date = date.split("T")[0]
     year, month, day = separate_date.split("-")
     return f"{day}.{month}.{year}"
