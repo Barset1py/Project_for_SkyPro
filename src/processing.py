e7bcdc5ebba15b09d337060ed09482e589aad7b8
барахтин Сергей <smoke13tub@gmail.com>
2025-09-14 15:55:11 +0800

    добавлена функция сортировки данных по параметру

---
+++ src/processing.py
@@ -0,0 +1,12 @@
+from src import widget
+
+def filter_by_state(data: list[dict[str, str]], state: str='EXECUTED') -> list[dict[str, str]]:
+    '''Функция сортирует список по параметру "state"'''
+    sorted_list = []
+    for item in data:
+        if item['state'] == state:
+            sorted_list.append(item)
+    return sorted_list
+
+
+def sort_by_date(data: list[dict[str, str]], reverse_sorted: bool=False) -> list[dict[str, str]]:
