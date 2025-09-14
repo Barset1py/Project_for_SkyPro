553a8d7e73ae531de8ccfd0050556d7df4d468d2
барахтин Сергей <smoke13tub@gmail.com>
2025-09-14 16:05:18 +0800

    мелкие поправления в коде

--- src/processing.py
+++ src/processing.py
@@ -1,18 +1,15 @@
 from src import widget
 
-def filter_by_state(data: list[dict[str, str]], state: str='EXECUTED') -> list[dict[str, str]]:
+
+def filter_by_state(data: list[dict[str, str]], state: str = "EXECUTED") -> list[dict[str, str]]:
     '''Функция сортирует список по параметру "state"'''
     sorted_list = []
     for item in data:
-        if item['state'] == state:
+        if item["state"] == state:
             sorted_list.append(item)
     return sorted_list
 
 
-def sort_by_date(data: list[dict[str, str]], reverse_sorted: bool=False) -> list[dict[str, str]]:
-    '''Функция сортирует список по дате от большего к меньшему'''
-    return sorted(
-        data,
-        key=lambda x: widget.get_date(x['date']),  # Используем преобразованную дату
-        reverse=reverse_sorted
-    )
+def sort_by_date(data: list[dict[str, str]], reverse_sorted: bool = True) -> list[dict[str, str]]:
+    """Функция сортирует список по дате от меньшего к большему"""
+    return sorted(data, key=lambda x: widget.get_date(x["date"]), reverse=reverse_sorted)
