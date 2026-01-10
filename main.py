278d5a27d994f1519da580b251a9d06bb5a99013
Сергей Барахтин <smoke13tub@gmail.com>
2026-01-10 17:27:14 +0900

    fix func call

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ print(f"Маска номера карты: {masks.get_mask_card_number(input_nu
 print(f"Маска номера счета: {masks.get_mask_account(input_account_number)}\n")
 
 print("Введите номер счета или карты")
-input_numbers_account = input("В формате <имя счета или карты> <номер карты или счета>: ")
-print(widget.mask_account_card(input_numbers_account))
+print(widget.mask_account_card())
 
 print("Введите дату")
 format_date = input("В формате <2025-01-01T00:00:00.000000>: ")
