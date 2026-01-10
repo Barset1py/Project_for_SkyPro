eca88ac2b1b2e16f14e243f5848be5bd5927d526
Сергей Барахтин <smoke13tub@gmail.com>
2026-01-10 17:25:16 +0900

    fix func mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -1,10 +1,15 @@
 from src import masks
 
 
-def mask_account_card(number: str) -> str:
+def mask_account_card():
     """Функия накладывает маску на номер карты или счета в зависимости от длины номера"""
     result = None
-    separate_num = number.split()
+    input_numbers_account = input("В формате <имя счета или карты> <номер карты или счета>: ")
+    while not input_numbers_account:
+        print('Поле ввода пустое!')
+        input_numbers_account = input("В формате <имя счета или карты> <номер карты или счета>: ")
+
+    separate_num = input_numbers_account.split()
     if len(separate_num[-1]) == 16:
         result = ' '.join(separate_num[:-1]) + " " + masks.get_mask_card_number(separate_num[-1])
     else:
