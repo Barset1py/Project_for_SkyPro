5c661f8a45ef4d9d79ad7963731eebbf89d00217
Сергей Барахтин <smoke13tub@gmail.com>
2026-02-13 16:38:18 +0900

    fix fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,7 +31,7 @@ def transactions_with_missing_state():
 
 @pytest.fixture
 def exec_only():
-    """Ожидаемый результат для state == 'EXECUTED' (из BASE_TRANSACTIONS)."""
+    """Ожидаемый результат для state == 'EXECUTED'."""
     return [
         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
@@ -40,7 +40,7 @@ def exec_only():
 
 @pytest.fixture
 def canceled_only():
-    """Ожидаемый результат для state == 'CANCELED' (из BASE_TRANSACTIONS)."""
+    """Ожидаемый результат для state == 'CANCELED'."""
     return [
         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
@@ -55,7 +55,7 @@ def canceled_only():
 ])
 def test_cases(request, exec_only, canceled_only):
     """
-    Параметризованная фикстура: готовит входные данные и ожидаемый результат
+    Параметризованная фикстура готовит входные данные и ожидаемый результат
     для каждого сценария.
     """
     case_name, filter_state = request.param
