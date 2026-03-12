0c98bb4c41eaf8aca2150a59c32bcf6870859c21
Сергей Барахтин <smoke13tub@gmail.com>
2026-03-12 20:27:34 +0900

    fix code

--- src/widget.py
+++ src/widget.py
@@ -5,20 +5,20 @@ def mask_account_card(input_numbers_account: str) -> str:
     """Функия накладывает маску на номер карты или счета в зависимости от длины номера"""
     result = None
     while not input_numbers_account:
-        print('Поле ввода пустое!\nВведите номер карты или счета.')
+        print("Поле ввода пустое!\nВведите номер карты или счета.")
         input_numbers_account = input("В формате <имя счета или карты> <номер карты или счета>: ")
     separate_num = input_numbers_account.split()
     if len(separate_num[-1]) == 16:
-        result = ' '.join(separate_num[:-1]) + " " + masks.get_mask_card_number(separate_num[-1])
+        result = " ".join(separate_num[:-1]) + " " + masks.get_mask_card_number(separate_num[-1])
     else:
-        result = ' '.join(separate_num[:-1]) + " " + masks.get_mask_account(separate_num[-1])
+        result = " ".join(separate_num[:-1]) + " " + masks.get_mask_account(separate_num[-1])
     return result
 
 
 def get_date(format_date: str) -> str:
     """Функция переводит дату из одного формата в другой"""
     while not format_date:
-        print('Поле ввода пустое!\nВведите дату.')
+        print("Поле ввода пустое!\nВведите дату.")
         format_date = input("В формате <2025-01-01T00:00:00.000000>: ")
     separate_date = format_date.split("T")[0]
     year, month, day = separate_date.split("-")
